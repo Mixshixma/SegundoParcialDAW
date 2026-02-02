be8bd48cb7aab4fae49f54872781a2616bedea51
Mixshixma <herlenisalive@gmail.com>
2026-02-02 17:34:16 -0500

    Update index.php

--- Backend/index.php
+++ Backend/index.php
@@ -1,7 +1,2 @@
 <?php
-header("Content-Type: application/json");
-echo json_encode([
-    "mensaje" => "Bienvenido a la API de Carteles Anónimos",
-    "estado" => "En funcionamiento",
-    "documentacion" => "Usa los endpoints correspondientes para gestionar anuncios."
-]);
+echo json_encode(["status" => "online", "message" => "API funcionando correctamente"]);
