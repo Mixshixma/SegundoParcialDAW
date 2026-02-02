a87261d87dc350d75896329b260dfcf9e8a43f35
Mixshixma <herlenisalive@gmail.com>
2026-02-02 17:38:54 -0500

    Update validaciones.js

--- Frontend/js/validaciones.js
+++ Frontend/js/validaciones.js
@@ -15,7 +15,7 @@ document.getElementById('formAnuncio').addEventListener('submit', async function
     const formData = new FormData(this);
     
     // Aquí pondrás la URL que te asigne Render al desplegar tu PHP
-    const urlBackend = "https://backend-anuncios-yyc4.onrender.com/app/controllers/AnuncioController.php?action=crear";
+    const urlBackend = "https://anuncios-php.onrender.com//app/controllers/AnuncioController.php?action=crear";
 
     try {
         const response = await fetch(urlBackend, {
@@ -38,3 +38,4 @@ document.getElementById('formAnuncio').addEventListener('submit', async function
     }
 
 });
+
